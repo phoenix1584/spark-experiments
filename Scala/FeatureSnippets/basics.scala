426d9b199114d54979a802e230e74739d367dca1
phoenix1584 <phoenix1584@users.noreply.github.com>
2018-11-29 14:30:26 +0530

    refs #1 : Added sample functions and method.

--- Scala/FeatureSnippets/basics.scala
+++ Scala/FeatureSnippets/basics.scala
@@ -1,9 +1,27 @@
 object HelloWorld{
+    // A Method 
+    def add (x:Int,y:Int):Int  = x+ y
+
+    // Method with parameter list
+    def addThenMultiply(x:Int,y:Int)(multiplier:Int) = add(x,y) * multiplier
+
     def main(args: Array[String]):  Unit = {
         println("Hello world from Scala.")
 
+        // Val (immutable) and Var (mutable)
         val testVal = 100
         var testVar = 10 + testVal
         println(testVar,testVal)
+
+        // Sample function.
+        val sqr = (x:Int) => x * x
+        println(sqr(testVal))
+
+        // Calling a method
+        println(add(testVal,testVar))
+
+        // Calling method with param list
+        println(addThenMultiply(testVal,testVar)(testVar))
+        
     }
 }

57c3a72567ed1a6d695c5d59ea8af526f06a6984
phoenix1584 <phoenix1584@users.noreply.github.com>
2018-12-07 17:27:22 +0530

    refs #1: mixins example

---
+++ Scala/FeatureSnippets/mixins.scala
@@ -0,0 +1,24 @@
+object Mixins extends App{
+
+  def BasicMixins() : Unit = {
+    abstract class A {
+      val message : String
+    }
+
+    class B extends A {
+      val message = "I'm an instance of class B."
+    }
+
+    trait C extends A {
+      def loudMessage = message.toUpperCase()
+    }
+
+    class D extends B with C
+    
+    val d = new D
+    println(d.message)
+    println(d.loudMessage)
+  }
+}
+
+Mixins.main(args)
